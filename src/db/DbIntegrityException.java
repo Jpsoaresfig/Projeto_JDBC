a013c4ce2ff61944570e44d2b61ea8580ea0c3f1
unknown <jpbusinesss@hotmail.com>
2025-02-04 20:22:25 -0300

    primeiro commit e teste com banco

---
+++ src/db/DbIntegrityException.java
@@ -0,0 +1,14 @@
+package db;
+
+public class DbIntegrityException extends RuntimeException{
+	
+	private static final long serialVersionUID = 1l;
+	
+	public DbIntegrityException(String msg) {
+		
+		super(msg);
+	}
+	
+	
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Projeto_JDBC {
+	requires java.sql;
+}
