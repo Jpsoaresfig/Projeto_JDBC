c60775ffa20985091767629bec11145b5f18d708
unknown <jpbusinesss@hotmail.com>
2025-02-11 15:28:43 -0300

    implementando update

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,13 @@ public class Program {
 		Seller newSeller = new Seller(null, "greg", "greg@gmail.com", new Date(), 4000.0, department);
 		SellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = "+ newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = SellerDao.findById(1);
+		seller.setName("Martha Waine");
+		SellerDao.update(seller);
+		System.out.println("Update completed");
+		
 	
 	}
 		
